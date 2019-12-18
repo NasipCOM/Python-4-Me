a1eac5631af519cd8b683ed9a468b3b476499f67
NasipCOM <salmahunov@mail.ru>
2019-12-18 18:00:54 +0600

    Inputs

--- INPUTS.py
+++ INPUTS.py
@@ -1,6 +1,16 @@
 #With given size 
+
 arrLen = int(input())
 array=list(map(int, input().split()[:arrLen]))
 
 #Without given size 
+
 array=list(map(int, input().split()))
+
+#2 inputs strings or input
+
+number = input().strip()
+number, length = number.split(' ')
+
+# print(largest_product(number, int(length)))
+
